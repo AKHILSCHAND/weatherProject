49b279ed78b38fa9a83507e873b149f3aa59b592
akhilschand <nandhuakhil95@gmail.com>
2023-09-19 20:12:10 +0530

    views created

--- weatherApp/urls.py
+++ weatherApp/urls.py
@@ -6,3 +6,15 @@ from weatherApp import views
 urlpatterns = [
     path('',views.home,name='home'),
 ]
+
+
+# <form action="" method="post" class="col-md-6 col-md-offset-3">
+#     {% csrf_token %}
+#     <div class="input-group">
+#       <input type="text" class="form-control" name="city" placeholder="Search">
+#       <div class="input-group-btn">
+#         <button class="btn btn-default" type="submit">
+#             <span class="input-group-text border fw-bold" id="search-addon">Check!</span>
+#         </button>
+#       </div>
+#       <form>
--- weatherApp/views.py
+++ weatherApp/views.py
@@ -1,5 +1,32 @@
 from django.shortcuts import render
+import urllib.request
+import json
 
 # Create your views here.
 def home(request):
-    return render(request, 'home.html')
+    if request.method == 'POST':
+        city = request.POST.get('city').strip()  # Remove leading/trailing spaces
+        if city !="":
+            api_url = urllib.request.urlopen('http://api.openweathermap.org/data/2.5/weather?q=' + urllib.parse.quote(city) + '&units=metric&appid=987e31fd75f978dd2df3cdf8da52234e').read()
+            api_url_dict = json.loads(api_url)
+
+            data = {
+                "country": city,
+                "weather_description": api_url_dict['weather'][0]['description'],
+                "weather_temperature": api_url_dict['main']['temp'],
+                "weather_pressure": api_url_dict['main']['pressure'],
+                "weather_humidity": api_url_dict['main']['humidity'],
+                "weather_icon": api_url_dict['weather'][0]['icon'],
+            }
+            context = {
+                "city": city,
+                "data": data,
+                
+            }
+        else:
+            return render(request, 'home.html')
+    else:
+        city = None
+        data = {}
+
+    return render(request, 'home.html', context)
